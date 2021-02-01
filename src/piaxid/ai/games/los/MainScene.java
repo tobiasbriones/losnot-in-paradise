ce48c3eed48dffd9aeea4615dc3efaeb99ba3cb7
Tobias Briones <tobiasbrionesdev@gmail.com>
2021-01-31 18:15:25 -0600

    Update copyright, add the BSD-3-Clause License notice header

--- src/piaxid/ai/games/los/MainScene.java
+++ src/piaxid/ai/games/los/MainScene.java
@@ -1,7 +1,13 @@
 /*
  * Copyright (c) 2018-2021 Tobias Briones. All rights reserved.
  *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * This file is part of Losnot in Paradise.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
  */
 
 package piaxid.ai.games.los;
